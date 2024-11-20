4619cb0a04f373fcd28feb3b816472fa271cbabb
liangling <liangling@tencent.com>
2024-11-20 11:25:39 +0800

    feat: grafana链接与监控dashboard链接完全匹配 --Story=120566380

--- public/app/features/dashboard-scene/pages/DashboardScenePageStateManager.ts
+++ public/app/features/dashboard-scene/pages/DashboardScenePageStateManager.ts
@@ -90,11 +90,12 @@ export class DashboardScenePageStateManager extends StateManagerBase<DashboardSc
         const currentPath = locationService.getLocation().pathname;
         if (dashboardUrl !== currentPath) {
           // Spread current location to persist search params used for navigation
-          // locationService.replace({
-          //   ...locationService.getLocation(),
-          //   pathname: dashboardUrl,
-          // });
-          locationService.replace(currentPath);
+          locationService.replace({
+            ...locationService.getLocation(),
+            pathname: dashboardUrl,
+          });
+          window.parent?.postMessage({ pathname: dashboardUrl }, '*');
+          // locationService.replace(currentPath);
           console.log('not correct url correcting', dashboardUrl, currentPath);
         }
       }
